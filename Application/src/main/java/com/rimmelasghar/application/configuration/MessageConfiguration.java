567c8eb86d8beba10aa019b59ba82ef861e972e8
Rimmel Asghar <rimmelasghar3@gmail.com>
2023-07-23 14:57:32 +0500

    added configuration files for the project

---
+++ Application/src/main/java/com/rimmelasghar/application/configuration/MessageConfiguration.java
@@ -0,0 +1,53 @@
+package com.rimmelasghar.application.configuration;
+
+
+import com.rimmelasghar.application.utils.ProjectConstants;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class MessageConfiguration {
+
+	@Bean
+	MessageSource generalMessageSource() {
+
+		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/messages/general/GeneralMessages");
+		messageSource.setDefaultEncoding(ProjectConstants.DEFAULT_ENCODING);
+
+		return messageSource;
+	}
+
+	@Bean
+	MessageSource exceptionMessageSource() {
+
+		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/messages/exception/ExceptionMessages");
+		messageSource.setDefaultEncoding(ProjectConstants.DEFAULT_ENCODING);
+
+		return messageSource;
+	}
+
+	@Bean
+	public MessageSource validationMessageSource() {
+
+		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/messages/validation/ValidationMessages");
+		messageSource.setDefaultEncoding(ProjectConstants.DEFAULT_ENCODING);
+
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean getValidator() {
+
+		final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(validationMessageSource());
+
+		return bean;
+	}
+
+}
