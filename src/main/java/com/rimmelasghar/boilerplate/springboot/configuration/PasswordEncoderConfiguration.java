8fb929b2e5c207b2d70ecafd18c187d97a669899
Rimmel Asghar <rimmelasghar3@gmail.com>
2023-07-27 13:56:15 +0500

    added files for Configuration

---
+++ src/main/java/com/rimmelasghar/boilerplate/springboot/configuration/PasswordEncoderConfiguration.java
@@ -0,0 +1,16 @@
+package com.rimmelasghar.boilerplate.springboot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+public class PasswordEncoderConfiguration {
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
